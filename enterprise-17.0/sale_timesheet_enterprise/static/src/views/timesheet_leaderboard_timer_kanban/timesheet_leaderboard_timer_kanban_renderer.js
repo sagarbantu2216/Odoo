c18e5e86a8314318b9556411d457edabcd9404fd
sagarbantu2216 <sagar@edvenswatech.com>
2024-11-13 07:03:14 +0530

    initial commit

--- enterprise-17.0/sale_timesheet_enterprise/static/src/views/timesheet_leaderboard_timer_kanban/timesheet_leaderboard_timer_kanban_renderer.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module **/
-
-import { TimesheetTimerKanbanRenderer } from "@timesheet_grid/views/timesheet_kanban/timesheet_timer_kanban_renderer";
-import { patchRenderer } from "../patch_renderer";
-
-patchRenderer(TimesheetTimerKanbanRenderer);
--- enterprise-17.0/sale_timesheet_enterprise/static/src/views/timesheet_leaderboard_timer_list/timesheet_leaderboard_timer_list_renderer.js
+++
@@ -1,6 +0,0 @@
-/** @odoo-module **/
-
-import { TimesheetTimerListRenderer } from "@timesheet_grid/views/timesheet_list/timesheet_timer_list_renderer";
-import { patchRenderer } from "../patch_renderer";
-
-patchRenderer(TimesheetTimerListRenderer);
